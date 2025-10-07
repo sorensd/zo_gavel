fea19f149a4bde7f778c4cceca2ae815fe6dd54f
Soren Singh Dary <67230851+sorensd@users.noreply.github.com>
2025-10-08 04:28:08 +0530

    Update client.lua

--- client.lua
+++ client.lua
@@ -7,7 +7,7 @@ CreateThread(function()
 	hasPlayerLoaded = true
 end)
 
--- Client event to handle gavel sounds
+-- Client event to handle gavel sounds (No changes needed here)
 RegisterNetEvent('zogavel:play_gavel_sound', function(soundFile, soundVolume)
     if hasPlayerLoaded then
         SendNUIMessage({
@@ -18,34 +18,45 @@ RegisterNetEvent('zogavel:play_gavel_sound', function(soundFile, soundVolume)
     end
 end)
 
--- Set ox_target BoxZone
-exports.ox_target:addBoxZone({
-    coords = Config.gavelLocation, 
-    size = vec3(1, 1, 1),
-    rotation = 45,
-    debug = false,
-    options = {
-        {
-            -- groups = "judge", -- ADD PROPER JOB NAME
-            name = "Gavel",
-            event = "zogavel:play_gavel_sound_server",
-            icon = "fa-solid fa-gavel",
-            label = "Gavel - one time",
-            soundType = "one"
+-- Create the qb-target BoxZone
+CreateThread(function()
+    -- Wait for the config to be loaded, just in case
+    while Config.gavelLocation == nil do
+        Wait(100)
+    end
+
+    exports['qb-target']:AddBoxZone("judge_gavel", Config.gavelLocation, 1.0, 1.0, {
+        name = "judge_gavel",
+        heading = 45,
+        minZ = Config.gavelLocation.z - 1.0,
+        maxZ = Config.gavelLocation.z + 1.0,
+        debugPoly = false,
+    }, {
+        options = {
+            {
+                type = "client",
+                event = "zogavel:play_gavel_sound_server",
+                icon = "fa-solid fa-gavel",
+                label = "Gavel - one time",
+                job = "judge", -- ADD PROPER JOB NAME
+                soundType = "one" -- Custom parameter passed to the event
+            },
+            {
+                type = "client",
+                event = "zogavel:play_gavel_sound_server",
+                icon = "fa-solid fa-gavel",
+                label = "Gavel - three times",
+                job = "judge", -- ADD PROPER JOB NAME
+                soundType = "three" -- Custom parameter passed to the event
+            },
         },
-        {
-            -- groups = "judge", -- ADD PROPER JOB NAME
-            name = "Gavel",
-            event = "zogavel:play_gavel_sound_server",
-            icon = "fa-solid fa-gavel",
-            label = "Gavel - three times",
-            soundType = "three"
-        }
-    }
-})
+        distance = 1.5 -- The distance from which the target can be seen
+    })
+end)
 
--- EventHandler for clicking inside of the BoxZone
-AddEventHandler("zogavel:play_gavel_sound_server", function(targetData)
-    TriggerServerEvent('zogavel:play_gavel_sound', targetData.soundType)
 
+-- EventHandler for clicking the target option (No changes needed here)
+RegisterNetEvent("zogavel:play_gavel_sound_server", function(data)
+    -- qb-target passes the 'soundType' parameter from the selected option in the 'data' table
+    TriggerServerEvent('zogavel:play_gavel_sound', data.soundType)
 end)
